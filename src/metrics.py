62e7b7e94010f29962ca6e9a67c689daa56b0579
= <=>
2025-06-06 14:26:02 +0700

    Comment out plotting functions in metrics.py to disable unused features

--- src/metrics.py
+++ src/metrics.py
@@ -6,7 +6,7 @@
 # import warnings
 from pathlib import Path
 
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import numpy as np
 import threading
 import os
@@ -1154,52 +1154,52 @@ def get_label_json(lb_file, class_dict):
 
 
 
-# Plots ----------------------------------------------------------------------------------------------------------------
+# # Plots ----------------------------------------------------------------------------------------------------------------
 
 
-@threaded
-def plot_pr_curve(px, py, ap, save_dir=Path("pr_curve.png"), names=()):
-    """Plots precision-recall curve, optionally per class, saving to `save_dir`; `px`, `py` are lists, `ap` is Nx2
-    array, `names` optional.
-    """
-    fig, ax = plt.subplots(1, 1, figsize=(9, 6), tight_layout=True)
-    py = np.stack(py, axis=1)
+# @threaded
+# def plot_pr_curve(px, py, ap, save_dir=Path("pr_curve.png"), names=()):
+#     """Plots precision-recall curve, optionally per class, saving to `save_dir`; `px`, `py` are lists, `ap` is Nx2
+#     array, `names` optional.
+#     """
+#     fig, ax = plt.subplots(1, 1, figsize=(9, 6), tight_layout=True)
+#     py = np.stack(py, axis=1)
 
-    if 0 < len(names) < 21:  # display per-class legend if < 21 classes
-        for i, y in enumerate(py.T):
-            ax.plot(px, y, linewidth=1, label=f"{names[i]} {ap[i, 0]:.3f}")  # plot(recall, precision)
-    else:
-        ax.plot(px, py, linewidth=1, color="grey")  # plot(recall, precision)
+#     if 0 < len(names) < 21:  # display per-class legend if < 21 classes
+#         for i, y in enumerate(py.T):
+#             ax.plot(px, y, linewidth=1, label=f"{names[i]} {ap[i, 0]:.3f}")  # plot(recall, precision)
+#     else:
+#         ax.plot(px, py, linewidth=1, color="grey")  # plot(recall, precision)
 
-    ax.plot(px, py.mean(1), linewidth=3, color="blue", label=f"all classes {ap[:, 0].mean():.3f} mAP@0.5")
-    ax.set_xlabel("Recall")
-    ax.set_ylabel("Precision")
-    ax.set_xlim(0, 1)
-    ax.set_ylim(0, 1)
-    ax.legend(bbox_to_anchor=(1.04, 1), loc="upper left")
-    ax.set_title("Precision-Recall Curve")
-    fig.savefig(save_dir, dpi=250)
-    plt.close(fig)
+#     ax.plot(px, py.mean(1), linewidth=3, color="blue", label=f"all classes {ap[:, 0].mean():.3f} mAP@0.5")
+#     ax.set_xlabel("Recall")
+#     ax.set_ylabel("Precision")
+#     ax.set_xlim(0, 1)
+#     ax.set_ylim(0, 1)
+#     ax.legend(bbox_to_anchor=(1.04, 1), loc="upper left")
+#     ax.set_title("Precision-Recall Curve")
+#     fig.savefig(save_dir, dpi=250)
+#     plt.close(fig)
 
 
-@threaded
-def plot_mc_curve(px, py, save_dir=Path("mc_curve.png"), names=(), xlabel="Confidence", ylabel="Metric"):
-    """Plots a metric-confidence curve for model predictions, supporting per-class visualization and smoothing."""
-    fig, ax = plt.subplots(1, 1, figsize=(9, 6), tight_layout=True)
+# @threaded
+# def plot_mc_curve(px, py, save_dir=Path("mc_curve.png"), names=(), xlabel="Confidence", ylabel="Metric"):
+#     """Plots a metric-confidence curve for model predictions, supporting per-class visualization and smoothing."""
+#     fig, ax = plt.subplots(1, 1, figsize=(9, 6), tight_layout=True)
 
-    if 0 < len(names) < 21:  # display per-class legend if < 21 classes
-        for i, y in enumerate(py):
-            ax.plot(px, y, linewidth=1, label=f"{names[i]}")  # plot(confidence, metric)
-    else:
-        ax.plot(px, py.T, linewidth=1, color="grey")  # plot(confidence, metric)
+#     if 0 < len(names) < 21:  # display per-class legend if < 21 classes
+#         for i, y in enumerate(py):
+#             ax.plot(px, y, linewidth=1, label=f"{names[i]}")  # plot(confidence, metric)
+#     else:
+#         ax.plot(px, py.T, linewidth=1, color="grey")  # plot(confidence, metric)
 
-    y = smooth(py.mean(0), 0.05)
-    ax.plot(px, y, linewidth=3, color="blue", label=f"all classes {y.max():.2f} at {px[y.argmax()]:.3f}")
-    ax.set_xlabel(xlabel)
-    ax.set_ylabel(ylabel)
-    ax.set_xlim(0, 1)
-    ax.set_ylim(0, 1)
-    ax.legend(bbox_to_anchor=(1.04, 1), loc="upper left")
-    ax.set_title(f"{ylabel}-Confidence Curve")
-    fig.savefig(save_dir, dpi=250)
-    plt.close(fig)
+#     y = smooth(py.mean(0), 0.05)
+#     ax.plot(px, y, linewidth=3, color="blue", label=f"all classes {y.max():.2f} at {px[y.argmax()]:.3f}")
+#     ax.set_xlabel(xlabel)
+#     ax.set_ylabel(ylabel)
+#     ax.set_xlim(0, 1)
+#     ax.set_ylim(0, 1)
+#     ax.legend(bbox_to_anchor=(1.04, 1), loc="upper left")
+#     ax.set_title(f"{ylabel}-Confidence Curve")
+#     fig.savefig(save_dir, dpi=250)
+#     plt.close(fig)

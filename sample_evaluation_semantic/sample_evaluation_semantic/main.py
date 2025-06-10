5a7a87efa9cf052d546703e486b4bd0003d1025f
= <=>
2025-06-10 15:15:30 +0700

    update

---
+++ sample_evaluation_semantic/sample_evaluation_semantic/main.py
@@ -0,0 +1,55 @@
+import os
+from metrics import *
+from model import *
+from PIL import Image
+import time
+import numpy as np
+
+if __name__ == "__main__":
+    #ONLY CHANGE 5 LINES
+    data_path = './sample_test' #path to dataset
+    nc = 10 #number of class
+    path_to_model = "./model.tflite" #path to model
+    model = Model(model_path=path_to_model) 
+    dataset_name = "dataset" #must be one of these names: "lars", "rescuenet", "loveda"
+    #
+
+    input_size = 320
+    model.prepare()
+
+    image_paths = get_image_paths(data_path, dataset_name)
+    total_time = 0.0
+    total_file = len(image_paths)
+    results = []
+    targets = get_target_from_data(data_path, dataset_name, input_size)
+
+    for fi in image_paths:
+        print(fi)
+        img = Image.open(fi).resize((input_size, input_size))
+        labels = targets[os.path.basename(fi).rsplit(".", 1)[0]]
+
+        start_time = time.time()
+        preds = model.predict(img)
+        stop_time = time.time()
+        run_time = stop_time - start_time
+        total_time += run_time
+        results.append((preds, labels))
+
+    FPS = total_file/total_time
+    print("Average FPS: {:.3f}".format(FPS))
+    normFPS = FPS/100
+    f1 = eval_semantic_results(results, nc)
+    score = 2*normFPS*f1/(normFPS + f1)
+    print("F1: {:.3f}".format(f1))
+    print("Score: {:.3f}".format(score))
+    
+
+
+
+
+
+
+
+
+
+
---
+++ sample_evaluation_semantic/sample_evaluation_semantic/model.py
@@ -0,0 +1,47 @@
+import numpy as np
+import tflite_runtime.interpreter as tflite
+
+class Model(object):
+    def __init__(self, model_path):
+        #super.__init__()
+        self.interpreter = tflite.Interpreter(model_path=model_path)
+        self.interpreter.allocate_tensors()
+
+        self.BOX_COORD_NUM = 4
+
+        self.input_details = self.interpreter.get_input_details()
+        self.output_details = self.interpreter.get_output_details()
+
+        # check the type of the input tensor
+        self.floating_model = self.input_details[0]["dtype"] == np.float32
+
+        # NxHxWxC, H:1, W:2
+        self.input_height = self.input_details[0]["shape"][1]
+        self.input_width = self.input_details[0]["shape"][2]
+
+        self.max_box_count = self.output_details[0]["shape"][2]
+
+        self.class_count = self.output_details[0]["shape"][1] - self.BOX_COORD_NUM
+        self.input_mean = 0.0
+        self.input_std = 255.0
+        self.keypoint_count = 0
+        self.score_threshold = 0.6
+
+    def prepare(self):
+        return None
+
+    def predict(self, image):
+        input_data = np.expand_dims(image, axis=0)
+
+        if self.floating_model:
+            input_data = (np.float32(input_data) - self.input_mean) / self.input_std
+
+        self.interpreter.set_tensor(self.input_details[0]["index"], input_data)
+
+        self.interpreter.invoke()
+
+        output_data = self.interpreter.get_tensor(self.output_details[0]["index"])
+        results = np.squeeze(output_data).transpose()
+
+        return results
+
